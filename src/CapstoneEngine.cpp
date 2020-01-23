6e5fab8aadadc07a99df6277ea40072a5492a4c8
Alec Roberts <plstriagin@gmail.com>
2020-01-22 23:08:48 -0800

    Initial Commit

---
+++ src/CapstoneEngine.cpp
@@ -0,0 +1,12 @@
+/*
+ * Capstone Engine developed by Alec Roberts and Steven Cole.
+ * Started Jan 22 2020
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
