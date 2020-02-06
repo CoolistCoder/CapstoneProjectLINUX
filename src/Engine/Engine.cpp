74f10f4f782b4acff190366d09d48d3060d53478
Alec Roberts <plstriagin@gmail.com>
2020-02-06 14:19:52 -0800

    Implemented the initialization step

--- src/Engine/Engine.cpp
+++ src/Engine/Engine.cpp
@@ -3,7 +3,54 @@
 #include "Engine.h"
 
 void Engine::init(){
+	//first, begin the process of initialization
+	//because we want to initialize SDL as a whole for now,
+	//we'll use the everything flag for initializing SDL2
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0){
+		//failure to init occurs if the value of SDL_Init is less than 0
+		//we want to show a simple message box that will tell the user that something went wrong
+		SDL_ShowSimpleMessageBox(
+		SDL_MESSAGEBOX_ERROR,	//create a messagebox with an error icon
+		"ERROR",				//the title of the error should just be "error" to describe what kind of box it is
+		"UNABLE TO INIT SDL",	//a simple description of the error
+		nullptr					//our window is currently null, so we should just use nullptr
+		);
+		//we must break from the init
+		return;
+	}
 
+	//if the first init phase works, then we can move onto step two
+	//we must initialize the image module
+	if (IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG)<1){
+		//IMG init fails if the value returned is 0
+		//the error message is largely the same
+		SDL_ShowSimpleMessageBox(
+		SDL_MESSAGEBOX_ERROR,	//create a messagebox with an error icon
+		"ERROR",				//the title of the error should just be "error" to describe what kind of box it is
+		"UNABLE TO INIT SDL IMAGE",	//a simple description of the error
+		nullptr					//our window is currently null, so we should just use nullptr
+		);
+		//break the function
+		return;
+	}
+
+	//the final step is similar to the first two steps
+	//we must initialize the mixer module
+	if (Mix_Init(MIX_INIT_FLAC | MIX_INIT_MP3 | MIX_INIT_OGG)<1){
+		//MIX init also fails if the value returned is 0
+		//the error message is still mostly the same
+		SDL_ShowSimpleMessageBox(
+		SDL_MESSAGEBOX_ERROR,	//create a messagebox with an error icon
+		"ERROR",				//the title of the error should just be "error" to describe what kind of box it is
+		"UNABLE TO INIT SDL MIXER",	//a simple description of the error
+		nullptr					//our window is currently null, so we should just use nullptr
+		);
+		//break the function
+		return;
+	}
+
+	//if all of these initialization processes have been successfully run, we can set the success variable to true
+	this->success = true;
 }
 
 void Engine::end(){
@@ -52,7 +99,7 @@ Engine::Engine() {
 	this->context = 0; //don't need the context yet
 	this->window = nullptr;		//no window has been created
 	this->isrunning = false;	//the window is not running, therefore this is false for now
-	this->success = true;	//this is set to true by default, but if anything wrong happens during init, change it
+	this->success = false;	//the engine has not been initialized, therefore it is not successfully initialized
 	this->framerate = 60;	//by default, the engine runs at 60 hrz
 
 	this->init();	//perform the init after the engine's default values have been set
