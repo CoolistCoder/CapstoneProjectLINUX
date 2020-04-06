14a69fe484f5a350f14111e27cb95ca0941b0e0e
Alec Roberts <plstriagin@gmail.com>
2020-04-05 21:57:05 -0700

    Updated background code

--- src/Engine/Background.cpp
+++ src/Engine/Background.cpp
@@ -6,118 +6,175 @@ void Background::defaultBehavior(Entity* e)
 }
 
 void Background::draw() {
+	//we only want to implement the draw if the background has data
+	if (!this->empty()) {
+		//these values determine whether or not to use the renderer or custom values
+		int drawToX, drawToY, drawToW, drawToH;
+		//these values determine whether or not to use the image's original height and width
+		int drawSubX, drawSubY, drawSubW, drawSubH;
 
-	glMatrixMode(GL_TEXTURE); //the texture matrix will be the matrix we will perform our operations on
-	glLoadIdentity(); //load the identity matrix
+		//assign X and Y to the object's data
+		drawToX = this->x;
+		drawToY = this->y;
 
-	//adjust the matrix to the size of our image
-	glScaled(1.0 / this->storedSource->w, 1.0 / this->storedSource->h, 1.0); //the scale will be derived from the image dimensions
+		//if we want to use the renderer's data:
+		if (this->toRenderer && this->getEngine()) {
+			drawToW = this->getEngine()->getResW(); //get the renderer's width
+			drawToH = this->getEngine()->getResH(); //get the renderer's height
+		}
+		else { //if we can't get the renderer's information, simply set W and H to the stored values
+			drawToW = this->w;
+			drawToH = this->h;
+		}
 
-	//now use modelview for drawing the image
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+		//if we want to use the image's data:
+		if (this->directTexture && !this->empty()) {
+			//set the subimage data to size of the image
+			drawSubX = 0;
+			drawSubY = 0;
+			drawSubW = this->storedSource->w;
+			drawSubH = this->storedSource->h;
+		}
+		else {
+			//use the custom subimage data
+			drawSubX = this->textureX;
+			drawSubY = this->textureY;
+			drawSubW = this->textureW;
+			drawSubH = this->textureH;
+		}
 
-	//set up the parameters for drawing the image so it uses the pixels
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		//we need to get some data
+		const int total_subimages = this->framesW * this->framesH; //total subimages made from the image
 
-	//bind this texture to our image
-	glBindTexture(GL_TEXTURE_2D, this->image);
+		//we need to be able to calculate what subimages to grab based on the image index
+		//make sure the frame value isn't higher than the number of total images we have
+		while (this->frame >= total_subimages) {
+			//subtract 1 from frame until we have a valid index
+			this->frame--;
+		}
 
-	//enable texture drawing
-	glEnable(GL_TEXTURE_2D);
+		glMatrixMode(GL_TEXTURE); //the texture matrix will be the matrix we will perform our operations on
+		glLoadIdentity(); //load the identity matrix
 
-	if (flagX) {
-		glBegin(GL_QUADS);
-			glTexCoord2i(this->storedSource->w, 0);				//top left of texture
-			glVertex2i(0, 0);	//top left of background
+		//adjust the matrix to the size of our image
+		glScaled(1.0 / this->storedSource->w, 1.0 / this->storedSource->h, 1.0); //the scale will be derived from the image dimensions
 
-			glTexCoord2i(0, 0);				//top right of texture
-			glVertex2i(this->knownEngine->getResW(), 0);	//top right of background
+		//now use modelview for drawing the image
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
 
-			glTexCoord2i(0, this->storedSource->h);				//bottom right of texture
-			glVertex2i(this->knownEngine->getResW(), this->knownEngine->getResH());	//bottom right of background
+		//apply if the rotation isn't zero
+		if (this->rotation != 0) {
+			//translate so center is at (0,0,0)
+			glTranslatef(drawToX + this->modposX + (drawToW / 2), drawToY + this->modposY + (drawToH / 2), 0);
 
-			glTexCoord2i(this->storedSource->w, this->storedSource->h);				//bottom left of texture
-			glVertex2i(0, this->knownEngine->getResH());	//bottom left of background
-		glEnd();
+			//rotate the background a specified angle around the z-axis (2-D rotation)
+			glRotatef(this->rotation, 0.0, 0.0, 1.0);
+
+			//translate back to original position
+			glTranslatef(-(drawToX + this->modposX + (drawToW / 2)), -(drawToY + this->modposY + (drawToH / 2)), 0);
+		}
+
+		//we need to account for culling if a flip is to occur
+		if (this->toggleFlipX || this->toggleFlipY) { //change the culling
+			glFrontFace(GL_CCW); //cull counterclockwise if a flip occurs
+		}
+		if (this->toggleFlipX && this->toggleFlipY) {
+			glFrontFace(GL_CW); //cull clockwise if a double flip is present
+		}
+
+		//perform a horizontal flip if the xFlip bool is true
+		if (this->toggleFlipX) {
+			//translate so center is at (0,0,0)
+			glTranslatef(drawToX + this->modposX + (drawToW / 2), drawToY + this->modposY + (drawToH / 2), 0);
+
+			//rotate the background a specified angle around the z-axis (2-D rotation)
+			glRotatef(180, 0.0, 1.0, 0.0);
+
+			//translate back to original position
+			glTranslatef(-(drawToX + this->modposX + (drawToW / 2)), -(drawToY + this->modposY + (drawToH / 2)), 0);
+		}
+
+		//perform a vertical flip if the yFlip bool is true
+		if (this->toggleFlipY) {
+			//translate so center is at (0,0,0)
+			glTranslatef(drawToX + this->modposX + (drawToW / 2), drawToY + this->modposY + (drawToH / 2), 0);
+
+			//rotate the background a specified angle around the z-axis (2-D rotation)
+			glRotatef(180, 1.0, 0.0, 0.0);
+
+			//translate back to original position
+			glTranslatef(-(drawToX + this->modposX + (drawToW / 2)), -(drawToY + this->modposY + (drawToH / 2)), 0);
+		}
+
+		//set up the parameters for drawing the image so it uses the pixels
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		//bind this texture to our image
+		glBindTexture(GL_TEXTURE_2D, this->image);
+
+		//use a surrogate variable to determine whether or not the camera follows
+		int cameraPosX, cameraPosY;
+		//determine if the background follows the camera
+		if (this->independentFromCamera) { //if they are independent, set to 0
+			cameraPosX = 0;
+			cameraPosY = 0;
+		}
+		else { //if they are not independent, just make them the same as the camera
+			cameraPosX = this->modposX;
+			cameraPosY = this->modposY;
+		}
+
+		//enable texture drawing
+		glEnable(GL_TEXTURE_2D);
+
+			glBegin(GL_QUADS);
+					glTexCoord2i(drawSubX, drawSubY);				//top left of subimage
+					glVertex2i(drawToX + cameraPosX, drawToY + cameraPosY);	//top left of background
+
+					glTexCoord2i(drawSubX + drawSubW, drawSubY);				//top right of subimage
+					glVertex2i(drawToX + cameraPosX + drawToW, drawToY + cameraPosY);	//top right of background
+
+					glTexCoord2i(drawSubX + drawSubW, drawSubY + drawSubH);				//bottom right of subimage
+					glVertex2i(drawToX + cameraPosX + drawToW, drawToY + cameraPosY + drawToH);	//bottom right of background
+
+					glTexCoord2i(drawSubX, drawSubY + drawSubH);				//bottom left of subimage
+					glVertex2i(drawToX + cameraPosX, drawToY + cameraPosY + drawToH);	//bottom left of background
+			glEnd();
+
+
+		glMatrixMode(GL_TEXTURE);
+		glLoadIdentity();
+
+		//must disable textures to prevent OpenGL mixing up the textures as we go
+		glDisable(GL_TEXTURE_2D);
 	}
-	else if (flagY) {
-		glBegin(GL_QUADS);
-			glTexCoord2i(0, this->storedSource->h);				//top left of texture
-			glVertex2i(0, 0);	//top left of background
-
-			glTexCoord2i(this->storedSource->w, this->storedSource->h);				//top right of texture
-			glVertex2i(this->knownEngine->getResW(), 0);	//top right of background
-
-			glTexCoord2i(this->storedSource->w, 0);				//bottom right of texture
-			glVertex2i(this->knownEngine->getResW(), this->knownEngine->getResH());	//bottom right of background
-
-			glTexCoord2i(0, 0);				//bottom left of texture
-			glVertex2i(0, this->knownEngine->getResH());	//bottom left of background
-		glEnd();
-	}
-	else if (flagX && flagY) {
-		glBegin(GL_QUADS);
-			glTexCoord2i(this->storedSource->w, this->storedSource->h);				//top left of texture
-			glVertex2i(0, 0);	//top left of background
-
-			glTexCoord2i(0, this->storedSource->h);				//top right of texture
-			glVertex2i(this->knownEngine->getResW(), 0);	//top right of background
-
-			glTexCoord2i(0, 0);				//bottom right of texture
-			glVertex2i(this->knownEngine->getResW(), this->knownEngine->getResH());	//bottom right of background
-
-			glTexCoord2i(this->storedSource->w, 0);				//bottom left of texture
-			glVertex2i(0, this->knownEngine->getResH());	//bottom left of background
-		glEnd();
-	}
-	else {
-		glBegin(GL_QUADS);
-			glTexCoord2i(0, 0);				//top left of texture
-			glVertex2i(0, 0);	//top left of background
-
-			glTexCoord2i(this->storedSource->w, 0);				//top right of texture
-			glVertex2i(this->knownEngine->getResW(), 0);	//top right of background
-
-			glTexCoord2i(this->storedSource->w, this->storedSource->h);				//bottom right of texture
-			glVertex2i(this->knownEngine->getResW(), this->knownEngine->getResH());	//bottom right of background
-
-			glTexCoord2i(0, this->storedSource->h);				//bottom left of texture
-			glVertex2i(0, this->knownEngine->getResH());	//bottom left of background
-		glEnd();
-	}
-
-	glMatrixMode(GL_TEXTURE);
-	glLoadIdentity();
-
-	//must disable textures to prevent OpenGL mixing up the textures as we go
-	glDisable(GL_TEXTURE_2D);
 }
 
 void Background::flipX() {
-	if (this->flagX == false)
-		this->flagX = true;
-	else this->flagX = false;
+	if (this->toggleFlipX == false)
+		this->toggleFlipX = true;
+	else this->toggleFlipX = false;
 }
 
 void Background::flipY() {
-	if (this->flagY == false)
-		this->flagY = true;
-	else this->flagY = false;
+	if (this->toggleFlipY == false)
+		this->toggleFlipY = true;
+	else this->toggleFlipY = false;
 }
 
-//void Background::rotate(int angle) {
-//	//translate so center is at (0,0,0)
-//	glTranslatef(-this->centerX, -this->centerY, 0);
-//
-//	//rotate the background a specified angle around the z-axis (2-D rotation)
-//	glRotatef(angle, 0.0, 0.0, 1.0);
-//
-//	//translate back to original position
-//	glTranslatef(this->centerX, this->centerY, 0);
-//
-//}
+void Background::setRotation(int angle) {
+	this->rotation = angle;
+}
+
+void Background::setToRenderSize() {
+	this->toRenderer= true;
+}
+
+void Background::detachFromRenderer() {
+	this->toRenderer = false;
+}
 
 void Background::setPosition(int x, int y) {
 	//set the x and y position of the background
@@ -125,10 +182,22 @@ void Background::setPosition(int x, int y) {
 	this->y = y;
 }
 
+void Background::setSubimage(int x, int y, int w, int h) {
+	//simply assign these values to the texture retrieval data
+	this->textureX = x;
+	this->textureY = y;
+	this->textureW = w;
+	this->textureH = h;
+}
+
 void Background::setSize(unsigned int w, unsigned int h) {
-	//set the width and height of background
-	this->w = w;
-	this->h = h;
+	//set the width and height of background (can't be less than 0)
+	if (w > 0) {
+		this->w = w;
+	}
+	if (h > 0) {
+		this->h = h;
+	}
 }
 
 void Background::execute() {
@@ -136,19 +205,43 @@ void Background::execute() {
 }
 
 Background::Background() {
+	//initialize position to 0
 	this->x = 0;
 	this->y = 0;
-	this->w = 1;
-	this->h = 1;
 
-	//this->centerX = (this->knownEngine->getResW() / 2);
-	//this->centerY = (this->knownEngine->getResH() / 2);
+	//by default, width and height are set to some default value
+	this->w = 640;
+	this->h = 480;
 
-	this->flagX = false;
-	this->flagY = false;
+	//make this a direct texture by default
+	this->directTexture = true;
 
+	//by default, the background does not scroll with the camera
+	this->independentFromCamera = false;
+
+	//just get a single pixel
+	this->setSubimage(0,0,1,1);
+
+	//set the frame to 0, we have no information regarding that yet
+	this->frame = 0;
+
+	//DOESN"T WORK
+	//obtain the center of the background
+	this->centerX = (this->w / 2);
+	this->centerY = (this->h / 2);
+
+	//by default, no flip should occur
+	this->toggleFlipX = false;
+	this->toggleFlipY = false;
+
+	//by default, the number of subimages accross and down would be 1
+	this->framesW = 1;
+	this->framesH = 1;
+
+	//by default, the background is visible
 	this->visible = true;
 
+	//set the default behavior of the background
 	this->setBehavior(Background::defaultBehavior);
 }
 
--- src/Engine/Background.h
+++ src/Engine/Background.h
@@ -7,32 +7,55 @@
 #include "Image.h"
 
 class Background : public Entity , public Image {
-private:
+protected:
 	int x, y; //the x and y positions of the background (should be the same as renderer)
 	unsigned int w, h; //the width and height of the background
-	int centerX, centerY;
-	bool visible; //to toggle visibility of background
-	bool flagX, flagY;
+	int centerX, centerY; //center of the renderer
+	unsigned int framesW, framesH; //the number of horizontal and vertical frames
 
-	//int subX, subY;
-	//unsigned int subW, subH;
+	int textureX, textureY, textureW, textureH; //these are the subimage variables!
+
+	int rotation; //the angle by which the background is rotated
+
+	bool visible; //to toggle visibility of background
+	bool toggleFlipX, toggleFlipY; //flags indicate whether or not to perform a flip
+	bool toRenderer;
+	bool directTexture; //boolean determining if the texture should stretch directly to the edges of the object
+	bool independentFromCamera; //determines if the background scrolls with the camera
+
+	int frame; //the frame the background is drawn at
 
 
 	static void defaultBehavior(Entity* e); //the default behavior of the background
 
 public:
 	void draw(); //draw the background
-	void flipX();
-	void flipY();
-	//void rotate(int);
+
+	void flipX(); //flip the background over the horizontal axis
+	void flipY(); //flip the background over the vertical axis
+	void setRotation(int);  //rotate the image by a specified angle
+
+	void setToRenderSize();
+	void detachFromRenderer(); //detaches the object from the renderer
+
+	void useImage() { this->directTexture = true; }; //this lets us use our image values
+	void useSubimage() { this->directTexture = false; }; //this lets us use our subimage values
 
 	void setPosition(int, int); //set position of the background
 	int getX() { return this->x; }; //return the x position of the background
 	int getY() { return this->y; }; //return the y position of the background
 
+	void setSubimage(int,int,int,int);
+
+	//camera functions
+	void hasCamera() { this->independentFromCamera = false; }; //attaches the object to the active camera
+	void noCamera() { this->independentFromCamera = true; }; //detaches the object from the active camera
+
+	//other stuff
 	void setSize(unsigned int, unsigned int);  //set the size of the background
 	unsigned int getW() { return this->w; }; //return the width of the background
 	unsigned int hetH() { return this->h; }; //retuen the height of the background
+
 	void hide() { this->visible = false; } //hides the background
 	void show() { this->visible = true; } //shows the background
 
