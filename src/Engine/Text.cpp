fcd28e2c0658e116aea72777c5e3801d20631858
Alec Roberts <plstriagin@gmail.com>
2020-04-25 21:05:26 -0700

    Began implementation of text object

---
+++ src/Engine/Text.cpp
@@ -0,0 +1,12 @@
+
+#include "Text.h"
+
+Text::Text() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Text::~Text() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Engine/Text.h
@@ -0,0 +1,12 @@
+
+
+#ifndef ENGINE_TEXT_H_
+#define ENGINE_TEXT_H_
+
+class Text {
+public:
+	Text();
+	virtual ~Text();
+};
+
+#endif /* ENGINE_TEXT_H_ */
